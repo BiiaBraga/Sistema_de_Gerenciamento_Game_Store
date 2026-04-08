dd013480ec6a1565ef9a6b977ad58ca0f9d5299a
Beatriz Braga <beatrizsagrado9@gmail.com>
2026-04-08 13:08:11 -0300

    feat: entidade produto e entidade categoria

--- src/categoria/entities/categoria.entity.ts
+++ src/categoria/entities/categoria.entity.ts
@@ -1,3 +1,23 @@
+//importações
+import { Column, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Produto } from "../../produto/entities/produto.entity";
+import { IsNotEmpty } from "class-validator";
+
 export class Categoria{
+
+    //atributos
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @IsNotEmpty()
+    @Column({length: 100, nullable:false})
+    nome!: string;
+
+    @IsNotEmpty()
+    @Column({length: 250, nullable:false})
+    descricao!: string;
+
+    @OneToMany(() => Produto, (produto) => produto.categoria)
+    produto!: Produto[];
     
 }
