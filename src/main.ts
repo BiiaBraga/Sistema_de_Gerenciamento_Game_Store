21edd4c67f753748cd80afc8fe3778df3a368fd7
Beatriz Braga <beatrizsagrado9@gmail.com>
2026-04-08 11:25:33 -0300

    feat: construção da base

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+//importações
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+//função de inicialização
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
